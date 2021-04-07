8811ea4405886d80866ace8fe9f58aefbb7da57e
shivamrai83 <shivamrai83@gmail.com>
2021-04-07 12:22:27 +0530

    logo removed

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
